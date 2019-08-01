c5d2fe813dfd0ec843b47e221467d8fba060240e
Gurjit Singh <g.s@outlook.in>
2019-08-01 18:40:35 +0530

    Initial commit

---
+++ AllowOutboundTemporarily.ps1
@@ -0,0 +1,79 @@
+
+#region Get-Admin
+function Get-Admin() {
+
+    Begin {
+    
+        # Catch All Errors
+        $ErrorActionPreference = "Stop"
+
+    }
+    
+    process {
+        if (!(([Security.Principal.WindowsIdentity]::GetCurrent()).Owner.Value.Equals("S-1-5-32-544"))) {
+        
+            try {
+
+                $ProcParam = @{
+                    FilePath     = 'powershell'
+                    ArgumentList = "-File `"$($MyInvocation.PSCommandPath)`" -NoExit"
+                    Verb         = 'runAs'
+
+                }
+
+                Start-Process @ProcParam
+
+            }
+            catch {
+    
+                Write-Output ("`n`nError: `n$($PSItem.Exception)`n" +
+                    "`n$($PSItem.CategoryInfo)`n")
+                
+                # requires Get-AnyKeyTo.psm1 module
+                Get-AnyKeyTo -msg "exit"
+            }
+            finally {
+                $ErrorActionPreference = "Continue"
+                Exit
+            }
+        }
+    
+    }
+    
+}
+
+#endregion
+
+#region Get-AnyKey
+function Get-AnyKeyTo ($msg) {
+    
+    Write-Output "Press any key to $msg..."
+    $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+}
+#endregion
+
+# Elevate privileges
+Get-Admin
+
+$ProfileParam = @{
+    All = $true
+    # DefaultOutboundAction = 'Allow'
+
+}
+
+$ProfileParam.Add("DefaultOutboundAction", "Allow")
+
+Set-NetFirewallProfile @ProfileParam
+
+Write-Host @"
+
+Outbound Traffic Allowed.
+
+"@
+
+Get-AnyKeyTo("revert back to block outbound")
+
+$ProfileParam.Set_Item("DefaultOutboundAction", "Block")
+
+Set-NetFirewallProfile @ProfileParam
